5751e5df305ff77eca121638e9ff281c5d5c4638
Yael <yaelbercow@gmail.com>
2016-06-25 22:07:25 -0400

    final commit

--- script.js
+++ script.js
@@ -4,15 +4,16 @@
 
 //For a multiple of 5, print "Buzz" instead of the number. 
 
-F//or a number that is a multiple of both 3 and 5, print "FizzBuzz" instead of the number.
+//or a number that is a multiple of both 3 and 5, print "FizzBuzz" instead of the number.
 
 function fizzBuzz(number) {
-	if (number % 3)== 0; {
-		return "Fizz"
-	}
-	if number % 5 == 0; {
-		return "Buzz"
-	} else if number % 3 == 0 &&  number % 5 == 0 {
-		return "FizzBuzz"
-	}
+  if (number % 15 === 0) {
+    return "FizzBuzz";
+  } else if (number % 3 === 0) {
+    return 'Fizz';
+  } else if (number % 5 === 0) {
+    return 'Buzz';
+  } else {
+    return number;
+  }
 }
